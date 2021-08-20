3c7c33c9a83c5edf808cfb0d4c53feaefdecc9fe
Benjamin Odisho <Semiroundpizza8@gmail.com>
2021-08-19 19:58:44 -0500

    removed ssl

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 const pg = require("pg");
-pg.defaults.ssl = true;
-
 // Create a connection to our DB
 const databaseName = "PuppyStore";
 const PORT = 5432;
 const client = new pg.Client(
-  process.env.DATABASE_URL || `postgres://localhost:${PORT}/${databaseName}`
+  process.env.DATABASE_URL || `postgres://localhost:${PORT}/${databaseName}`,
+  {
+    ssl: { rejectUnauthorized: false },
+  }
 );
 client.connect();
 
