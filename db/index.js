1a94514185b93692fe438ddd6ecbf52da24d15aa
Benjamin Odisho <Semiroundpizza8@gmail.com>
2021-08-19 20:03:46 -0500

    trying something

--- db/index.js
+++ db/index.js
@@ -2,12 +2,15 @@ const pg = require("pg");
 // Create a connection to our DB
 const databaseName = "PuppyStore";
 const PORT = 5432;
-const client = new pg.Client(
-  process.env.DATABASE_URL || `postgres://localhost:${PORT}/${databaseName}`,
-  {
-    ssl: { rejectUnauthorized: false },
-  }
-);
+
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || `postgres://localhost:${PORT}/${databaseName}`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 client.connect();
 
 module.exports = {
